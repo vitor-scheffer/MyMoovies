654e6f97d5fbc21c6dc75a1d6f3941324d068017
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-09-15 11:54:33 -0300

    Adicionando tableview.

---
+++ MyMoovies/Moovie.swift
@@ -0,0 +1,21 @@
+//
+//  Moovie.swift
+//  MyMoovies
+//
+//  Created by José Vitor Scheffer Boff dos Santos on 15/09/22.
+//
+
+import UIKit
+
+class Moovie {
+    
+    var tittle: String!
+    var description: String!
+    var img: UIImage!
+    
+    init( tittle: String, description: String ){
+        self.tittle = tittle
+        self.description = description
+    }
+    
+}
--- MyMoovies/ViewController.swift
+++ MyMoovies/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var moovies: [Moovie] = []
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var moovie: Moovie
+        moovie = Moovie(tittle: "Filme 1", description: "Descrição 1")
+        moovies.append( moovie )
+        moovie = Moovie(tittle: "Filme 2", description: "Descrição 2")
+        moovies.append( moovie )
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moovies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let moovie = moovies[ indexPath.row ]
+        let moovieContentReuse = "moovieContent"
+        
+        let moovieContent = tableView.dequeueReusableCell(withIdentifier: moovieContentReuse, for: indexPath)
+        moovieContent.textLabel?.text = moovie.tittle
+        
+        return moovieContent
+    }
 
 }
 
